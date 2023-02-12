d9e4c84593fb1a065baa159a2825046be5f1db91
YUVRAJSHARMA24 <yuvraj.june2002@gmail.com>
2023-02-12 19:13:46 +0530

    initial commit

---
+++ src/components/Login.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import styled from "styled-components";
+
+const Login = () => {
+  return (
+    <Container>
+      <Content>
+        <CTA>
+          <CtaLogoOne src="/images/cta-logo-one.svg" alt="" />
+          <SignUp>GET ALL THERE</SignUp>
+          <Description>
+            Subscribe to watch all content on Disney+ Hotstar
+          </Description>
+        </CTA>
+
+        <BgImage />
+      </Content>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  height: 100vh;
+`;
+
+const Content = styled.div`
+  margin-bottom: 10vw;
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 80px 40px;
+  height: 100%;
+`;
+
+const BgImage = styled.div`
+  height: 100%;
+  background-position: top;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url("/images/login-background.jpg");
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+`;
+
+const CTA = styled.div`
+  margin-bottom: 2vw;
+  max-width: 650px;
+  flex-wrap: wrap;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 0;
+  align-items: center;
+  text-align: center;
+  margin-right: auto;
+  margin-left: auto;
+  width: 100%;
+`;
+
+const CtaLogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
+`;
+
+const SignUp = styled.a`
+  @import url("https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap");
+  font-weight: bold;
+  color: #f9f9f9;
+  background-color: #0063e5;
+  margin-bottom: 12px;
+  width: 100%;
+  letter-spacing: 1.5px;
+  padding: 16.5px 0;
+  border: 1px solid transparent;
+  border-radius: 5px;
+
+  &:hover {
+    background-color: #0483ee;
+    cursor: pointer;
+  }
+`;
+
+const Description = styled.p`
+ letter-spacing: 1px;
+ color: hsla(0, 0%, 95.3%, 1);
+ font-size: 15px;
+ margin: 0 0 24px;
+`;
+
+export default Login;
