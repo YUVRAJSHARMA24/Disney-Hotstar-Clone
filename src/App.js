7abf36072a66f95c7e72678b21f5bcd6444efa84
YUVRAJSHARMA24 <yuvraj.june2002@gmail.com>
2023-02-13 17:08:36 +0530

    add header

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Login from './components/Login';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header/>
         <Routes>
           <Route path='/' element={<Login />} />
         </Routes>
---
+++ src/components/Header.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+  return <Nav>
+    <Logo>
+        <img src="/images/logo.svg" alt="" />
+    </Logo>
+    <NavMenu>Menu</NavMenu>
+  </Nav>;
+};
+
+const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 35px;
+  letter-spacing: 5px;
+  z-index: 3;
+`;
+
+const Logo = styled.a`
+    padding: 0;
+    width: 80px;
+    margin-top: 4px;
+    max-height: 70px;
+    font-size: 0;
+    display: inline-block;
+
+    img{
+        display: block;
+        width: 100%;
+    }
+`;
+
+const NavMenu = styled.div`
+    align-items: center;
+    display: flex;
+    flex-flow: row nowrap;
+    height: 100%;
+    justify-content: flex-end;
+    margin: 0;
+    padding: 0;
+    position: relative;
+    margin-right: auto;
+    margin-left: 25px;
+
+    @media (max-width: 768px) {
+        display: none;
+    }
+`;
+
+export default Header;
