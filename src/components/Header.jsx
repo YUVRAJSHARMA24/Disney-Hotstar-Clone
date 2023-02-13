518085dc17b3634e478876023923cf39d0afc78a
YUVRAJSHARMA24 <yuvraj.june2002@gmail.com>
2023-02-13 18:09:44 +0530

    change header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,47 @@
 import React from "react";
 import styled from "styled-components";
+import Login from "./Login";
 
 const Header = () => {
-  return <Nav>
-    <Logo>
+  return (
+    <Nav>
+      <Logo>
         <img src="/images/logo.svg" alt="" />
-    </Logo>
-    <NavMenu>Menu</NavMenu>
-  </Nav>;
+      </Logo>
+      <NavMenu>
+        <a href="/home">
+          <img src="/images/home-icon.svg" alt="" />
+          <span>HOME</span>
+        </a>
+
+        <a href="/search">
+          <img src="/images/search-icon.svg" alt="" />
+          <span>SEARCH</span>
+        </a>
+
+        <a href="/watchlist">
+          <img src="/images/WATCHLIST-icon.svg" alt="" />
+          <span>WATCHLIST</span>
+        </a>
+
+        <a href="/originals">
+          <img src="/images/original-icon.svg" alt="" />
+          <span>ORIGINALS</span>
+        </a>
+
+        <a href="/movies">
+          <img src="/images/movie-icon.svg" alt="" />
+          <span>MOVIES</span>
+        </a>
+
+        <a href="/series">
+          <img src="/images/series-icon.svg" alt="" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+      <LogIn>Login</LogIn>
+    </Nav>
+  );
 };
 
 const Nav = styled.nav`
@@ -26,34 +60,97 @@ const Nav = styled.nav`
 `;
 
 const Logo = styled.a`
-    padding: 0;
-    width: 80px;
-    margin-top: 4px;
-    max-height: 70px;
-    font-size: 0;
-    display: inline-block;
+  padding: 0;
+  width: 80px;
+  margin-top: 4px;
+  max-height: 70px;
+  font-size: 0;
+  display: inline-block;
 
-    img{
-        display: block;
-        width: 100%;
-    }
+  img {
+    display: block;
+    width: 100%;
+  }
 `;
 
 const NavMenu = styled.div`
-    align-items: center;
-    display: flex;
-    flex-flow: row nowrap;
-    height: 100%;
-    justify-content: flex-end;
-    margin: 0;
-    padding: 0;
-    position: relative;
-    margin-right: auto;
-    margin-left: 25px;
+  align-items: center;
+  display: flex;
+  flex-flow: row nowrap;
+  height: 100%;
+  justify-content: flex-end;
+  margin: 0;
+  padding: 0;
+  position: relative;
+  margin-right: auto;
+  margin-left: 25px;
 
-    @media (max-width: 768px) {
-        display: none;
+  @media (max-width: 768px) {
+    display: none;
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+
+    img {
+      height: 20px;
+      min-width: 20px;
+      width: 20px;
+      z-index: auto;
     }
+
+    span {
+      color: rgb(249, 249, 249);
+      font-size: 15px;
+      letter-spacing: 1px;
+      line-height: 1px;
+      padding: 2px 0;
+      white-space: nowrap;
+      position: relative;
+
+      &:before {
+        background-color: rgb(249, 249, 249);
+        border-radius: 0 0 4px 4px;
+        bottom: -10px;
+        content: "";
+        height: 2px;
+        left: 0px;
+        opacity: 0;
+        position: absolute;
+        right: 0px;
+        transform-origin: left center;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+        visibility: hidden;
+        width: auto;
+      }
+    }
+
+    &:hover {
+      span:before {
+        transform: scaleX(1);
+        visibility: visible;
+        opacity: 1 !important;
+      }
+    }
+  }
+`;
+
+const LogIn = styled.a`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid none;
+  border-radius: 4px;
+  transition: all 0.2s ease 0s;
+
+  &:hover {
+    background-color: #0063e5;
+    cursor: pointer;
+  }
 `;
 
 export default Header;
